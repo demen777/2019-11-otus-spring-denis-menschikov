b1eea7c49495d281fbb91b223380edb5f5fea670
demen <demen7@gmail.com>
2019-12-11 21:39:56 +0500

    Add Spring Boot application from start.spring.io

---
+++ hw3_testing/src/main/java/ru/otus/demen/hw3_testing/Hw3TestingApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.demen.hw3_testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw3TestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw3TestingApplication.class, args);
+	}
+
+}
---
+++ hw3_testing/src/test/java/ru/otus/demen/hw3_testing/Hw3TestingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.demen.hw3_testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw3TestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
