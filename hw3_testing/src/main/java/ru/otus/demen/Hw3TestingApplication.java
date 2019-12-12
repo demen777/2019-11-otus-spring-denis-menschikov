f09e39a36983d68102835717fcee4b1a5699a8c2
Меньщиков Денис Александрович <menshhikov-da@ural.rt.ru>
2019-12-12 16:42:49 +0500

    Home work 3 - spring boot

--- hw3_testing/src/main/java/ru/otus/demen/Hw3TestingApplication.java
+++ hw3_testing/src/main/java/ru/otus/demen/Hw3TestingApplication.java
@@ -2,12 +2,16 @@ package ru.otus.demen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import ru.otus.demen.service.TestingRunner;
 
 @SpringBootApplication
 public class Hw3TestingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Hw3TestingApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(Hw3TestingApplication.class, args);
+		TestingRunner testingRunner = context.getBean(TestingRunner.class);
+		testingRunner.run();
 	}
 
 }
--- hw3_testing/src/test/java/ru/otus/demen/Hw3TestingApplicationTests.java
+++ hw3_testing/src/test/java/ru/otus/demen/Hw3TestingApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Hw3TestingApplicationTests {
 
+	@SuppressWarnings("EmptyMethod")
 	@Test
 	void contextLoads() {
 	}
