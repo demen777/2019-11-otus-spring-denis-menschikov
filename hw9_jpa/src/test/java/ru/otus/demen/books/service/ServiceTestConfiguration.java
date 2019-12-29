2dc57c194b9449656730137812ae7c9f37b61c61
demen <demen7@gmail.com>
2019-12-29 09:26:35 +0500

    Change TestConfiguration to Configuration for ServiceTestConfiguration and add ComponentScan in services package

--- hw9_jpa/src/test/java/ru/otus/demen/books/service/ServiceTestConfiguration.java
+++ hw9_jpa/src/test/java/ru/otus/demen/books/service/ServiceTestConfiguration.java
@@ -2,12 +2,15 @@ package ru.otus.demen.books.service;
 
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import ru.otus.demen.books.dao.AuthorDao;
 import ru.otus.demen.books.dao.BookCommentDao;
 import ru.otus.demen.books.dao.BookDao;
 import ru.otus.demen.books.dao.GenreDao;
 
-@TestConfiguration
+@Configuration
+@ComponentScan("ru.otus.demen.books.service")
 public class ServiceTestConfiguration {
     @MockBean
     AuthorDao authorDao;
