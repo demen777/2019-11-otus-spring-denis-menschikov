9132302ac13b39592df006fa944a10dbf6acd924
Меньщиков Денис Александрович <menshhikov-da@ural.rt.ru>
2020-02-04 17:07:30 +0500

    Add add_author.html and mappings in AuthorController Add tests to AuthorControllerTest

--- hw16_classic_web/src/main/java/ru/otus/demen/books/controller/AuthorController.java
+++ hw16_classic_web/src/main/java/ru/otus/demen/books/controller/AuthorController.java
@@ -4,7 +4,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 import ru.otus.demen.books.controller.dto.mapper.AuthorDtoMapper;
 import ru.otus.demen.books.service.AuthorService;
 
@@ -24,6 +27,20 @@ public class AuthorController {
         modelAndView.addObject("authors",
             authorService.getAll().stream().map(authorDtoMapper::toAuthorDto).collect(Collectors.toList()));
         return modelAndView;
+    }
+
+    @GetMapping("/author/add")
+    public ModelAndView addAuthorGet() {
+        return new ModelAndView("add_author");
+    }
+
+    @PostMapping("/author/add")
+    public RedirectView addAuthorPost(@RequestParam("firstName") String firstName,
+                                      @RequestParam("surname") String surname)
+    {
+        log.info("addAuthorPost firstName={} surname={}", firstName, surname);
+        authorService.add(firstName, surname);
+        return new RedirectView("/authors");
     }
 }
 
--- hw16_classic_web/src/test/java/ru/otus/demen/books/controller/AuthorControllerTest.java
+++ hw16_classic_web/src/test/java/ru/otus/demen/books/controller/AuthorControllerTest.java
@@ -9,19 +9,22 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import ru.otus.demen.books.model.Author;
 import ru.otus.demen.books.service.AuthorService;
+import ru.otus.demen.books.service.exception.IllegalParameterException;
 
 import java.util.List;
 
 import static org.hamcrest.core.StringContains.containsString;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest(classes = ControllerTestConfiguration.class)
 @AutoConfigureMockMvc
 class AuthorControllerTest {
     private static final Author TOLSTOY = new Author(1L, "Лев", "Толстой");
+    public static final String FIRSTNAME_MUST_BE_NOT_EMPTY_MSG = "Имя не должно быть пустым";
+    public static final String SURNAME_MUST_BE_NOT_EMPTY_MSG = "Фамилия не должна быть пустой";
 
     @Autowired
     MockMvc mockMvc;
@@ -37,5 +40,52 @@ class AuthorControllerTest {
         resultActions.andExpect(status().isOk())
                 .andExpect(content().contentType("text/html;charset=UTF-8"))
                 .andExpect(content().string(containsString(TOLSTOY.getSurname())));
+    }
+
+    @Test
+    @DisplayName("Успешное отображение формы для ввода нового автора")
+    void addAuthorGet_ok() throws Exception {
+        ResultActions resultActions = mockMvc.perform(get("/author/add"));
+        resultActions.andExpect(status().isOk())
+            .andExpect(content().contentType("text/html;charset=UTF-8"));
+    }
+
+    @Test
+    @DisplayName("Успешное submit формы для ввода нового автора")
+    void addAuthorPost_ok() throws Exception {
+        ResultActions resultActions = mockMvc.perform(post("/author/add")
+            .param("firstName", TOLSTOY.getFirstName())
+            .param("surname", TOLSTOY.getSurname())
+        );
+        resultActions.andExpect(status().is3xxRedirection())
+            .andExpect(redirectedUrl("/authors"));
+        verify(authorService, times(1))
+            .add(TOLSTOY.getFirstName(), TOLSTOY.getSurname());
+    }
+
+    @Test
+    @DisplayName("Ввод нового автора с пустым именем")
+    void addAuthorPost_emptyFirstName() throws Exception {
+        when(authorService.add("", TOLSTOY.getSurname()))
+            .thenThrow(new IllegalParameterException(FIRSTNAME_MUST_BE_NOT_EMPTY_MSG));
+        ResultActions resultActions = mockMvc.perform(post("/author/add")
+            .param("firstName", "")
+            .param("surname", TOLSTOY.getSurname())
+        );
+        resultActions.andExpect(status().isOk())
+            .andExpect(content().string(containsString(FIRSTNAME_MUST_BE_NOT_EMPTY_MSG)));
+    }
+
+    @Test
+    @DisplayName("Ввод нового автора с пустой фамилией")
+    void addAuthorPost_emptySurname() throws Exception {
+        when(authorService.add(TOLSTOY.getFirstName(), ""))
+            .thenThrow(new IllegalParameterException(SURNAME_MUST_BE_NOT_EMPTY_MSG));
+        ResultActions resultActions = mockMvc.perform(post("/author/add")
+            .param("firstName", TOLSTOY.getFirstName())
+            .param("surname", "")
+        );
+        resultActions.andExpect(status().isOk())
+            .andExpect(content().string(containsString(SURNAME_MUST_BE_NOT_EMPTY_MSG)));
     }
 }
