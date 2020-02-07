aada6e80f502b1813f4e6168869abc1f643e777d
demen <demen7@gmail.com>
2020-02-07 15:56:00 +0500

    Add nav-pills and Redirect

--- hw17_modern_web_react/src/App.js
+++ hw17_modern_web_react/src/App.js
@@ -2,24 +2,28 @@ import React from 'react';
 import {
     Route,
     NavLink,
-    HashRouter
+    HashRouter, Redirect
 } from "react-router-dom";
 import Books from "./Books";
 import Authors from "./Authors";
 import Genres from "./Genres";
+import 'bootstrap/dist/css/bootstrap.css'
 
-class App extends React.Component {
+export default class App extends React.Component {
     render() {
         return (
             <HashRouter>
             <div className="App">
-                <nav className="menu">
-                    <NavLink to="/">Книги</NavLink>
-                    <NavLink to="/authors">Авторы</NavLink>
-                    <NavLink to="/genres">Жанры</NavLink>
+                <nav className="nav nav-pills">
+                    <NavLink className="nav-link" to="/books">Книги</NavLink>
+                    <NavLink className="nav-link" to="/authors">Авторы</NavLink>
+                    <NavLink className="nav-link" to="/genres">Жанры</NavLink>
                 </nav>
                 <div className="content">
-                    <Route exact path="/" component={Books}/>
+                    <Route exact path="/">
+                        <Redirect to="/books"/>
+                    </Route>
+                    <Route path="/books" component={Books}/>
                     <Route path="/authors" component={Authors}/>
                     <Route path="/genres" component={Genres}/>
                 </div>
@@ -28,5 +32,3 @@ class App extends React.Component {
         );
     }
 }
-
-export default App;
