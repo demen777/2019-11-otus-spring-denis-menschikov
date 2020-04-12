caec71c856e2c90afc3188570e87910c256508e5
demen <demen7@gmail.com>
2020-04-12 16:18:32 +0500

    Initial commit hw33resilience

---
+++ hw33_resilience4j/src/main/java/ru/otus/demen/hw33_resilience4j/Hw33Resilience4jApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.demen.hw33_resilience4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw33Resilience4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw33Resilience4jApplication.class, args);
+	}
+
+}
---
+++ hw33_resilience4j/src/test/java/ru/otus/demen/hw33_resilience4j/Hw33Resilience4jApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.demen.hw33_resilience4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw33Resilience4jApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
